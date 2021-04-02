5b39604104728fdf853588eee61527b1bd8311ea
Maciej Barta <maciej.barta@gmail.com>
2021-04-02 08:16:23 +0200

    set ignore in purgecss for preloader css

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,10 +102,10 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        develop: false, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ["/preloader.scss"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
